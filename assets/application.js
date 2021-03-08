8d4a00d53821ff9c3073e0b1a01753b9e9d1d6af
Ross Angus <ross.a.angus@gmail.com>
2021-03-08 16:14:28 +0000

    Added a check to see if the user is logged in as an admin or not. Removed some old classes.

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,6 @@
 // Put your application javascript here
+
+// Adds an attribute to the body tag, if the user is logged in as an admin
+setTimeout(function(){
+  if (document.getElementById('admin-bar-iframe')) document.body.setAttribute('data-admin','true');
+}, 2000);
